71964bf3b79d2fc4ea9695c9f6483bc22ae44c4d
Alexey Gronskiy <alexey@gronskiy.com>
2020-11-30 09:42:39 +0100

    Splitting

--- bin/deploy/deploy.sh
+++ bin/deploy/deploy.sh
@@ -4,37 +4,38 @@ set -e
 
 (
 
-curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd $curr_dir/../..
+    cd $curr_dir/../..
 
-if [ "`git status -s`" ]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+    if [ "`git status -s`" ]
+    then
+        echo "The working directory is dirty. Please commit any pending changes."
+        exit 1;
+    fi
 
-echo "Checking out gh-pages branch into public"
+    echo "Checking out gh-pages branch into public"
 
-cd public
-git checkout gh-pages
+    cd public
+    git fetch --all
+    git checkout origin/gh-pages
 
-git pull
+    git pull
 
-echo "Removing existing files"
-rm -rf *
+    echo "Removing existing files"
+    rm -rf *
 
-echo "Generating site"
-cd ..
-hugo
+    echo "Generating site"
+    cd ..
+    hugo
 
-echo "Updating gh-pages branch"
-cd public
-echo "gronskiy.com" > CNAME
-git add --all
-git commit -m "Publishing to gh-pages (publish.sh)"
+    echo "Updating gh-pages branch"
+    cd public
+    echo "gronskiy.com" > CNAME
+    git add --all
+    git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
-git push --all
+    # echo "Pushing to github"
+    git push --all
 
 )
--- bin/full-deploy-github.sh
+++ bin/full-deploy-github.sh
@@ -7,21 +7,20 @@ set -e
     curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     cd $curr_dir
 
-    if [ "`git status -s`" ]
-    then
-        echo "The working directory is dirty. Please commit any pending changes."
-        exit 1;
-    fi
-
     (
         cd ../content/posts
         if [ "`git status -s`" ]
-        then
-            echo "The `posts` directory is dirty. Please commit any pending changes."
-        exit 1;
-    fi
+            then
+                echo "The `posts` directory is dirty. Please commit any pending changes."
+            exit 1;
+        fi
     )
 
+    if [ "`git status -s`" ]
+        then
+            echo "The working directory is dirty. Please commit any pending changes."
+            exit 1;
+    fi
     echo "Generating tags"
     (
         ./generate-tags.sh
