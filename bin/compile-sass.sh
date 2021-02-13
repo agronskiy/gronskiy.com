4a65c3d99d53214b7d1c53b3c38a31d5c846862e
Alexey Gronskiy <alexey@gronskiy.com>
2021-02-13 22:33:42 +0100

    SASS

--- bin/compile-sass.sh
+++ bin/compile-sass.sh
@@ -8,8 +8,8 @@ curr_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $curr_dir
 
-sass -t compressed ../scss/main.scss ../themes/gron/static/css/main.min.css
-sass --scss ../scss/main.scss ../themes/gron/static/css/main.css
+sass -s compressed ../scss/main.scss ../themes/gron/static/css/main.min.css
+sass ../scss/main.scss ../themes/gron/static/css/main.css
 
 )
 
